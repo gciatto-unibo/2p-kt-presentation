bbee5fd3dc5dfae11b8b75cac21b93bdb280159f
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-04-30 13:07:47 +0200

    wip writing primitives examples

--- snippets/FindAll.kt
+++ snippets/FindAll.kt
@@ -1,23 +1,20 @@
-package it.unibo.tuprolog.solve.stdlib.primitive
-
-import it.unibo.tuprolog.core.*
-import it.unibo.tuprolog.solve.*
-import it.unibo.tuprolog.solve.primitive.*
-import it.unibo.tuprolog.unify.Unificator.Companion.mguWith
-
 object FindAll : TernaryRelation.NonBacktrackable<ExecutionContext>("findall") {
-    override fun Solve.Request<ExecutionContext>.computeOne(first: Term, second: Term, third: Term): Solve.Response {
+    override fun Solve.Request<ExecutionContext>.computeOne(
+        x: Term, y: Term, z: Term
+    ): Solve.Response {
         ensuringArgumentIsInstantiated(1)
         ensuringArgumentIsCallable(1)
         val solver = subSolver()
-        val solutions = solver.solve(second as Struct).toList()
-        val error = solutions.asSequence().filterIsInstance<Solution.Halt>().firstOrNull()
+        val solutions = solver.solve(y as Struct).toList()
+        val error = solutions.asSequence()
+            .filterIsInstance<Solution.Halt>()
+            .firstOrNull()
         if (error != null) {
             return replyException(error.exception.pushContext(context))
         }
         val mapped = solutions.asSequence()
             .filterIsInstance<Solution.Yes>()
-            .map { first[it.substitution].freshCopy() }
-        return replyWith(third mguWith List.of(mapped))
+            .map { x[it.substitution].freshCopy() }
+        return replyWith(z mguWith List.of(mapped))
     }
 }
--- snippets/Retract.kt
+++ snippets/Retract.kt
@@ -1,25 +1,18 @@
-package it.unibo.tuprolog.solve.stdlib.primitive
-
-import it.unibo.tuprolog.core.*
-import it.unibo.tuprolog.solve.ExecutionContext
-import it.unibo.tuprolog.solve.exception.error.PermissionError
-import it.unibo.tuprolog.solve.primitive.*
-import it.unibo.tuprolog.unify.Unificator.Companion.mguWith
-import it.unibo.tuprolog.utils.buffered
-
 object Retract : UnaryPredicate<ExecutionContext>("retract") {
-    override fun Solve.Request<ExecutionContext>.computeAll(first: Term): Sequence<Solve.Response> {
+    override fun Solve.Request<ExecutionContext>.computeAll(
+        x: Term
+    ): Sequence<Solve.Response> {
         ensuringArgumentIsWellFormedClause(0)
-        val clause: Clause = when (first) {
-            is Clause -> first
-            is Struct -> Rule.of(first, Var.anonymous())
+        val clause: Clause = when (x) {
+            is Clause -> x
+            is Struct -> Rule.of(x, Var.anonymous())
             else -> return sequenceOf(ensuringArgumentIsCallable(0).replyFail())
         }
         ensuringClauseProcedureHasPermission(clause, PermissionError.Operation.MODIFY)
         return context.dynamicKb[clause].buffered().map {
-            val substitution = when (first) {
-                is Clause -> (first mguWith it) as Substitution.Unifier
-                else -> (first mguWith it.head!!) as Substitution.Unifier
+            val substitution = when (x) {
+                is Clause -> (x mguWith it) as Substitution.Unifier
+                else -> (x mguWith it.head!!) as Substitution.Unifier
             }
             replySuccess(substitution) {
                 removeDynamicClauses(it)
